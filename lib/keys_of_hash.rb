f83cff9ac9c1c6c45b30a9d7d864178145304208
Ariel J <arieljakubowski@gmail.com>
2019-05-29 15:25:35 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,7 +3,6 @@ class Hash
   def keys_of(arguments)
     array = []
     arguments.each_with_index do |string, index|
-      binding.pry
       self.each do |species, location|
         if location == arguments[index]
           array << species
